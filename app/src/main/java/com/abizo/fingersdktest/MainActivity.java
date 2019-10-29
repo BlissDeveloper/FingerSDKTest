ac3a81b7bc0f5785a27ffb5f2945860b2c52f7e5
Avery <checkmate032@gmail.com>
2019-10-29 16:42:00 +0800

    -Added config for CPU on the project level gradle

--- app/src/main/java/com/abizo/fingersdktest/MainActivity.java
+++ app/src/main/java/com/abizo/fingersdktest/MainActivity.java
@@ -8,16 +8,24 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.aratek.trustfinger.sdk.DeviceOpenListener;
 import com.aratek.trustfinger.sdk.TrustFinger;
+import com.aratek.trustfinger.sdk.TrustFingerDevice;
+import com.cw.fpgabsdk.USBFingerManager;
 
 public class MainActivity extends AppCompatActivity {
     private String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
     private final int permissionCode = 5;
 
     public static final String TAG = "TAG";
+
+    int deviceIndex = 0;
+    TrustFingerDevice mTrustFingerDevice = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        closeSDK();
+    }
+
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
@@ -53,13 +67,40 @@ public class MainActivity extends AppCompatActivity {
 
     ////////////////////////////////////
 
+    public void closeSDK() {
+        mTrustFingerDevice.close();
+        USBFingerManager.getInstance(this).closeUSB();
+    }
+
     public void initSDK() {
-        try {
-            TrustFinger trustFinger = TrustFinger.getInstance(getApplicationContext());
-            trustFinger.initialize();
-        } catch (Exception e) {
-            Log.e(TAG, "Error initializing Trust Finger SDK. " + e.getMessage());
-        }
+        USBFingerManager.getInstance(this).setDelayMs(1000);
+        USBFingerManager.getInstance(this).openUSB(new USBFingerManager.OnUSBFingerListener() {
+            @Override
+            public void onOpenUSBFingerSuccess(String s, UsbManager usbManager, UsbDevice usbDevice) {
+                Log.d(TAG, "Successful");
+                TrustFinger mTrustfinger = TrustFinger.getInstance(getApplicationContext());
+                mTrustfinger.initialize();
+
+                mTrustfinger.openDevice(deviceIndex, new DeviceOpenListener() {
+                    @Override
+                    public void openSuccess(TrustFingerDevice device) {
+                        mTrustFingerDevice = device;
+                        Log.d(TAG, "Successful opening");
+                    }
+
+                    @Override
+                    public void openFail(String msg) {
+                        Log.e(TAG, "Opening device failed. " + msg);
+                    }
+                });
+            }
+
+            @Override
+            public void onOpenUSBFingerFailure(String s) {
+                Log.e(TAG, "Error on opening USB: " + s);
+            }
+        });
+
     }
 
     public void requestPermissions() {
@@ -74,5 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 == PackageManager.PERMISSION_GRANTED;
 
         return writePermission && readPermission;
+
+
     }
 }
